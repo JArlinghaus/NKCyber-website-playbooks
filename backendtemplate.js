31cf7fbfe8d804e4ed998fc7a8ab28709ec0d621
John Arlinghaus <18181026+JArlinghaus@users.noreply.github.com>
2019-08-15 04:29:25 -0400

    Added LDAPS to backend deployment

--- backendtemplate.js
+++ backendtemplate.js
@@ -8,6 +8,8 @@ module.exports = {
     env_production: {
       NODE_ENV: "production",
       PORT: {{ PORT }},
+      LDAPS: {{ LDAPS }},
+      LDAP_CERT_PATH: "{{ RELEASE_PATH }}/server/DC.pem",
       DB_IP: "{{ DB_IP }}",
       LDAP_BIND_IP: "{{LDAP_BIND_IP}}",
       LDAP_BIND_DN: "{{LDAP_BIND_DN}}",
